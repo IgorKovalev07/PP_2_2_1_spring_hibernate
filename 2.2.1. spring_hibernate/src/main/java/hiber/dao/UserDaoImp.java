c4c50b64b3de57adae4f33c9c75a059b4d579afc
IgorKovalev07 <iigorkovalev@gmail.com>
2025-04-01 14:06:10 +0300

    Add new

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,29 +1,37 @@
 package hiber.dao;
 
 import hiber.model.User;
+import jakarta.persistence.TypedQuery;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    public User getUserHasCar(String model, int series) {
+        String hql = "SELECT user FROM User user WHERE user.car.model = :model AND user.car.series = :series";
+        TypedQuery<User> typedQuery = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+        typedQuery.setParameter("model", model);
+        typedQuery.setParameter("series", series);
+        return typedQuery.getSingleResult();
+    }
 
 }
